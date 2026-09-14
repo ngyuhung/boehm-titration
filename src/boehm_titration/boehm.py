fixed multiple pipette uncertainty calculation

src/boehm_titration/boehm.py
@@ -13,9 +13,9 @@ class Boehm(_Titration):
 
         super().__init__(data, vmin, vmax)
 
-        self.vtotal: UFloat = ufloat(vtotal, vtotal/1e-3*U1000)
-        self.valiquot: UFloat = ufloat(valiquot, valiquot/1e-3*U1000)
-        self.vhcl: UFloat = ufloat(vhcl, vhcl/1e-3*U1000)
+        self.vtotal: UFloat = sum(ufloat(1e-3, U1000) for _ in range(int(np.ceil(vtotal/1e-3))))
+        self.valiquot: UFloat = sum(ufloat(1e-3, U1000) for _ in range(int(np.ceil(valiquot/1e-3))))
+        self.vhcl: UFloat = sum(ufloat(1e-3, U1000) for _ in range(int(np.ceil(vhcl/1e-3))))
 
         self.chcl: UFloat = chcl
         self.cnaoh: UFloat =  cnaoh

src/boehm_titration/standardisation.py
@@ -1,5 +1,6 @@
 from .settings import *
 from .titration import _Titration
+import numpy as np
 from uncertainties import ufloat, UFloat
 
 
@@ -12,8 +13,8 @@ class Standardisation(_Titration):
 
         self.desc = f'{X[unknown]} standardisation with {X[standard]}'
 
-        self.vanalyte = ufloat(vanalyte, vanalyte/1e-3*U1000)
-        self.vtitrant = ufloat(vtitrant, vtitrant/1e-3*U1000)
+        self.vanalyte = sum(ufloat(1e-3, U1000) for _ in range(int(np.ceil(vanalyte/1e-3))))
+        self.vtitrant = sum(ufloat(1e-3, U1000) for _ in range(int(np.ceil(vtitrant/1e-3))))
 
         if cstandard:
             self.cstandard = cstandard